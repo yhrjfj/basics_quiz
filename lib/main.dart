51371c68d4ffc36d0d0a0cee724a48fb170ff1a2
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-28 17:17:26 +0600

    Add 2 new screen and move some code from main to quiz screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:basics_quiz/start_screen.dart';
+import 'package:basics_quiz/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.blue,
-                Colors.blueGrey,
-              ],
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
