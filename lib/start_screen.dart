ccadac23bf3efd9ba157f66db6aceff81027aab0
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-28 14:50:01 +0600

    Add opacity into the flutter icon

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -9,17 +9,27 @@ class StartScreen extends StatelessWidget {
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
+          // 1st and recommended way for control opacity
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromRGBO(189, 195, 247, 0.795),
           ),
+          // // 2nd way to control opacity but not recommender
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(
             height: 80,
           ),
           const Text(
             'Want to join \'Flutter\' quiz?',
             style: TextStyle(
-              color: Color.fromRGBO(247, 253, 245, 0.992),
+              color: Color.fromRGBO(247, 253, 245, 0.808),
               fontSize: 24,
             ),
           ),
