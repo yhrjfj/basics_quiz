e1f1dbdc5e30b861afb22e62ecce1005ed9d7aa4
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-28 22:07:55 +0600

    Basic template for question

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -12,8 +12,33 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(context) {
-    return const Center(
-      child: Text('Question Screen'),
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 1'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 2'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 3'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer 4'),
+          ),
+        ],
+      ),
     );
   }
 }
