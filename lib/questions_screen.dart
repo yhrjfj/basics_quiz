e1e1dc5ed2f5d17786524e631d881060f9c44dd2
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-29 16:08:00 +0600

    Some minor change in question_screen

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -20,28 +20,33 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
     // Return
     return SizedBox(
       width: double.infinity,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            currentQuestion.text,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
+      child: Container(
+        margin: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.text,
+              style: const TextStyle(
+                color: Colors.white,
+                // fontSize: 15,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          ...currentQuestion.answers.map(
-            (answer) {
-              return AnswareButton(
-                answerText: answer,
-                onTap: () {},
-              );
-            },
-          ),
-        ],
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.answers.map(
+              (answer) {
+                return AnswareButton(
+                  answerText: answer,
+                  onTap: () {},
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
