6470b2154f841032ff744c01d4bdeca54bcf38be
Shinigami <102893295+shadowlight671@users.noreply.github.com>
2023-09-30 17:29:57 +0600

    Add map functionality for summarize user's answer

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,7 +1,30 @@
+import 'package:basics_quiz/data/questions.dart';
 import 'package:flutter/material.dart';
 
 class ResultsScreen extends StatelessWidget {
-  const ResultsScreen({super.key});
+  const ResultsScreen({
+    super.key,
+    required this.chosenAnswers,
+  });
+
+  final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_answer': questions[i].answers[0],
+          'user_answer': chosenAnswers[i],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
